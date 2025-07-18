e80d690b1787b0f220711a774b49dc5dd6ba99f9
Miguel Ángel Quisbert <quisbert.flores.miguel@gmail.com>
2025-07-17 23:47:10 -0300

    agregar config example

---
+++ config.example.php
@@ -0,0 +1,12 @@
+<?php
+// config.example.php
+
+$host = "localhost";
+$user = "TU_USUARIO";
+$password = "TU_PASSWORD";
+$db = "NOMBRE_BD";
+
+// Este archivo es un ejemplo de configuración.
+// No debe usarse directamente en producción.
+// Renombralo como config.php y completalo con tus datos reales.
+?>
